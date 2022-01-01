d66127e2404ba1dd95ec1f74531a8a6ebc9a5e66
SamariaSpringfield <94493448+SamariaSpringfield@users.noreply.github.com>
2021-12-31 21:11:11 -0700

    Update ContentView to You are successful

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Samaria!")
+        Text("You are successful Samaria!")
             .padding()
     }
 }
