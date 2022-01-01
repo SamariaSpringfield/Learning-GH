170e075984c53c512c9ed2603eff4724f043838b
Samaria Springfield <Samaria.springfield@gmail.com>
2021-12-31 20:50:15 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Samaria Springfield on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
